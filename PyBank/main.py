13e0dfe3a465b67ee186c30820d16a04583d8f48
Derell <derelljenkins@gmail.com>
2021-01-16 12:25:22 -0600

    Added csv header vairable

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,7 +2,7 @@
 
 import os
 import csv
-csvpath = os.path.join('../..', 'Resources', 'budget_data.csv')
+csvpath = os.path.join('Resources', 'budget_data.csv') 
 
 #Read in CSV and store header row
 
@@ -11,6 +11,9 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     
     print(csvreader)
+    #Store header 
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
 
 
 
