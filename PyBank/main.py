1d2e520b2dea22aa5d5aa3d5b89b85a9b0ea8566
Derell <derelljenkins@gmail.com>
2021-01-18 01:02:59 -0600

    Import date time module

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,22 +2,38 @@
 
 import os
 import csv
-csvpath = os.path.join('Resources', 'budget_data.csv') 
+
+py_path = os.path.join('Resources', 'budget_data.csv') 
+
+#import date time module
+from datetime import datetime
+
+first_date = datetime.strptime('Jan-2010', '%b-%Y')
+last_date = datetime.strptime('Feb-2017', '%b-%Y')
 
 #Read in CSV and store header row
 
-with open(csvpath) as csvfile:
+with open(py_path) as py_csv:
 
-    csvreader = csv.reader(csvfile, delimiter=',')
+    py_reader = csv.reader(py_csv, delimiter=',')
+    row_count = 0
     
-    print(csvreader)
+    print(py_reader)
     #Store header 
-    csv_header = next(csvreader)
+    csv_header = next(py_reader)
+   
     print(f"CSV Header: {csv_header}")
 
+    #Read each row after the header and print
+    for row in py_reader:
+        #csv_header = csv_header.replace("-", " ")
+        print(row)
 
 
 #Print title of result set with separator
 print('Finacial Analysis')
 print("----------------------")
 
+
+
+# %b Month as locale’s abbreviated name.    
